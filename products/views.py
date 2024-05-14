226c10e228982932c13d6e6774f6a42b9d608389
Anandhu4456 <anandhurajan56@gmail.com>
2024-05-14 12:30:18 +0530

    added pagination in product list

--- products/views.py
+++ products/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def list_products(request):
-    
-    products = Product.objects.all
-    
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1)
+    products = Product.objects.all()
+    pagination = Paginator(products,4)
+    products = pagination.get_page(page)
     return render(request, 'products_list.html',{"products":products})
 
 def single_product(request):
