11c0f1b952f2e96c7407858593b5a674df0db041
Anandhu4456 <anandhurajan56@gmail.com>
2024-05-14 16:18:58 +0530

    added account_banner and form

--- customers/views.py
+++ customers/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def account(request):
+    if request.POST and 'register' in request.POST:
+        user= request.POST.get('username')
+        phone = request.POST.get('phone')
+        email= request.POST.get('email')
+        password = request.POST.get('password')
     return render(request,'account.html')
