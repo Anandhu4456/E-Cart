0c126e4965b46a5dbd665fcab2ed263751b7a52b
Anandhu4456 <anandhurajan56@gmail.com>
2024-05-14 14:34:46 +0530

    added featured products and latest in home template

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name='home'),
     path('products/', views.list_products, name='list_products'),
-    path('single', views.single_product, name='single_product')
+    path('single/<pk>', views.single_product, name='single_product')
 ]
--- products/views.py
+++ products/views.py
@@ -3,16 +3,24 @@ from .models import Product
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    featured = Product.objects.order_by('priority')[:4]
+    latest = Product.objects.order_by('-id')[:4]
+    
+    context ={
+        "featured":featured,
+        "latest":latest
+    }
+    return render(request, 'index.html',context)
 
 def list_products(request):
     page=1
     if request.GET:
         page=request.GET.get('page',1)
-    products = Product.objects.all()
+    products = Product.objects.order_by('priority')
     pagination = Paginator(products,4)
     products = pagination.get_page(page)
     return render(request, 'products_list.html',{"products":products})
 
-def single_product(request):
-    return render(request, 'product_display.html')
+def single_product(request,pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'product_display.html',{"product":product})
